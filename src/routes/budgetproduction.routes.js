182014f2db259929dfc235547bb4bece94c332bc
morakot035 <morakm35@comexpert.co.th>
2026-06-17 15:11:01 +0700

    Update budgetproduction.routes.js

--- src/routes/budgetproduction.routes.js
+++ src/routes/budgetproduction.routes.js
@@ -5,15 +5,16 @@ import {
   getById,
   getPlants,
   getLines,
-  getTypes,
+  getLineCategories,
+  getProducts,
 } from "../controllers/budgetproduction.controller.js";
 
 const router = express.Router();
 
 router.get("/meta/plants", verifyToken, getPlants);
 router.get("/meta/lines", verifyToken, getLines);
-router.get("/meta/types", verifyToken, getTypes);
-
+router.get("/meta/line-categories", verifyToken, getLineCategories);
+router.get("/meta/products", verifyToken, getProducts);
 router.get("/", verifyToken, getAll);
 router.get("/:id", verifyToken, getById);
 
