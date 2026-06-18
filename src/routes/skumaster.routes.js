2c63d3ad60981acfd98665268e1cd4af2b382aeb
morakot035 <morakm35@comexpert.co.th>
2026-06-18 12:00:14 +0700

    Update skumaster.routes.js

--- src/routes/skumaster.routes.js
+++ src/routes/skumaster.routes.js
@@ -1,20 +1,68 @@
 import express from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
+
 import {
   getAll,
   getById,
   getPlants,
+  getPlantCodes,
   getLines,
+  getGroupProducts,
   getProductTypes,
+  getLineTypes,
+  getSizes,
 } from "../controllers/skumaster.controller.js";
 
 const router = express.Router();
 
+/*
+|--------------------------------------------------------------------------
+| Meta Data
+|--------------------------------------------------------------------------
+*/
+
 router.get("/meta/plants", verifyToken, getPlants);
+
+router.get(
+  "/meta/plant-codes",
+  verifyToken,
+  getPlantCodes,
+);
+
 router.get("/meta/lines", verifyToken, getLines);
-router.get("/meta/product-types", verifyToken, getProductTypes);
+
+router.get(
+  "/meta/group-products",
+  verifyToken,
+  getGroupProducts,
+);
+
+router.get(
+  "/meta/product-types",
+  verifyToken,
+  getProductTypes,
+);
+
+router.get(
+  "/meta/line-types",
+  verifyToken,
+  getLineTypes,
+);
+
+router.get(
+  "/meta/sizes",
+  verifyToken,
+  getSizes,
+);
+
+/*
+|--------------------------------------------------------------------------
+| SKU Master
+|--------------------------------------------------------------------------
+*/
 
 router.get("/", verifyToken, getAll);
+
 router.get("/:id", verifyToken, getById);
 
 export default router;
